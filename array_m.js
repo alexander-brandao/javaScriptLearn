3d85032c20ee0dc61e034cbca2969d9dfc505e1c
Alexander Brandao <alex12a7x@gmail.com>
2022-01-18 19:27:26 -0300

    array matriz

---
+++ array_m.js
@@ -0,0 +1,12 @@
+var lista_coisas = Array()
+
+lista_coisas['frutas'] = Array('Banana', 'maca', 'morango', 'uva')
+
+lista_coisas['pessoas'] = []
+
+lista_coisas['pessoas'][0] = 'alex'
+lista_coisas['pessoas'][1] = 'josh'
+lista_coisas['pessoas'][2] = 'sofia'
+
+document.write(lista_coisas['pessoas'][2])
+
