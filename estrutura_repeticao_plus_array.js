bbc154a75f600b69416e08711152a79fcba6faf7
Alexander Brandao <alex12a7x@gmail.com>
2022-01-28 10:36:49 -0300

    vamo bate essa meta

---
+++ estrutura_repeticao_plus_array.js
@@ -0,0 +1,16 @@
+var lista_pessoas = Array()
+
+lista_pessoas[0] = 'Alex'
+lista_pessoas[1] = 'Erica'
+lista_pessoas[2] = 'Julia'
+lista_pessoas[3] = 'Terezinha'
+lista_pessoas[4] = 'Nildo'
+lista_pessoas[5] = 'Italo'
+
+// variavel auxiliar
+var y = 0
+
+while(y < lista_pessoas.length){
+    document.write(lista_pessoas[y] + '<br />')
+    y++
+}
